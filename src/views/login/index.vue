9acd6ab29eed9dc54dd14e1bdfcb90e2a5a67ac3
yuan <duyuan9191@163.com>
2019-06-22 20:50:34 +0800

    登陆验证完成-俩天的时间才完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -15,11 +15,12 @@
             <el-input v-model="form.code" placeholder="验证码"></el-input>
           </el-col>
           <el-col :offset="1" :span="9">
-            <el-button>获取验证码</el-button>
+            <el-button @click="handleSendCode">获取验证码</el-button>
+            <!-- <el-button @click="handleSendCode">获取验证码</el-button> -->
           </el-col>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" class="btn-login">登陆</el-button>
+          <el-button type="primary" class="btn-login" @click="handleLogin">登陆</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -27,6 +28,9 @@
 </template>
 
 <script>
+import axios from 'axios'
+import '@/vendor/gt.js'
+
 export default {
   name: 'AppLogin',
   data () {
@@ -38,7 +42,70 @@ export default {
     }
   },
 
-  methods: {}
+  methods: {
+    handleLogin () {
+      axios({
+        method: 'POST',
+        url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
+        data: this.form
+      }).then(res => {
+        console.log(res.data)
+        this.$message({
+          message: '登录成功',
+          type: 'success'
+        })
+        this.$router.push({
+          name: 'home'
+        })
+      }).catch((e) => {
+        this.$message.error('登录失败,手机号或验证码错误')
+      })
+    },
+
+    handleSendCode () {
+      const { mobile } = this.form
+      axios({
+        method: 'GET',
+        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
+      }).then(res => {
+        // console.log(res.data)
+        const { data } = res.data
+        window.initGeetest({
+          gt: data.gt,
+          challenge: data.challenge,
+          offline: !data.success,
+          new_captcha: data.new_captcha,
+          product: 'bind' }, function (captchaObj) {
+          captchaObj.onReady(function () {
+            // 验证码ready之后才能调用verify方法显示验证码
+            captchaObj.verify() // 弹出验证码内容框
+          }).onSuccess(function () {
+            // your code
+            const {
+              geetest_challenge: challenge,
+              geetest_seccode: seccode,
+              geetest_validate: validate } =
+              captchaObj.getValidate()
+            axios({
+              method: 'GET',
+              url: `http://ttapi.research.itcast.cn/mp/v1_0/sms/codes/${mobile}`,
+              params: {
+                challenge,
+                validate,
+                seccode
+              }
+            }).then(res => {
+              console.log(res.data)
+            })
+          }).onError(function () {
+            // you code
+          })
+          // 在这里注册"发送验证码",按钮的的点击事件,然后验证用户是否输入手机号以及手机号的格式是否正确,没有问题:
+          // capchaObj.verify
+        })
+      })
+    }
+  }
 }
 </script>
 
@@ -52,7 +119,7 @@ export default {
   .login-head {
     display: flex;
     justify-content: center;
-    align-items: center;
+    align-items: center;   // 居中对齐弹性盒子的各项
     margin-bottom: 10px;
     img {
       width: 200px;
