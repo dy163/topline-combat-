18fedc5b53b6345499ec619f38c821ea68bf090f
yuan <duyuan9191@163.com>
2019-06-25 15:43:30 +0800

    登录退出功能完成

--- src/views/layout/components/AppHeader.vue
+++ src/views/layout/components/AppHeader.vue
@@ -4,14 +4,13 @@
     <el-col :span="5" :offset="9">
       <el-dropdown>
         <span class="el-dropdown-link">
-          <img width="30px" :src="userInfo.photo" >
+          <img width="30px" :src="userInfo.photo">
           {{ userInfo.name }}
           <i class="el-icon-arrow-down el-icon--right"></i>
         </span>
         <el-dropdown-menu slot="dropdown">
           <el-dropdown-item>个人设置</el-dropdown-item>
-          <el-dropdown-item>退出</el-dropdown-item>
-
+          <el-dropdown-item @click.native="handleLayout">退出</el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
     </el-col>
@@ -29,12 +28,39 @@ export default {
   created () {
     this.userInfo = JSON.parse(window.localStorage.getItem('user_info'))
   },
-  methods: {}
+  methods: {
+    handleLayout () {
+      console.log(123)
+      this.$confirm('确认退出?', '退出提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          // 清空本地存储中的userInfo
+          window.localStorage.removeItem('user_info')
+          // 跳转到登录页
+          this.$router.push({ name: 'login' })
+
+          this.$message({
+            type: 'success',
+            message: '退出成功!'
+          })
+        })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消退出'
+          })
+        })
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-.el-col, .el-dropdown-link {
+.el-col,
+.el-dropdown-link {
   height: 60px;
   display: flex;
   align-items: center;
